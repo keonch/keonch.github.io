5bfedc901874e7af37df6e55d3559283e4d5e2f8
Keon Choi <keonch91@gmail.com>
2021-04-19 15:40:51 -0400

    Updates

---
+++ static/js/main.c98105e8.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonpwebpage=this.webpackJsonpwebpage||[]).push([[0],{11:function(e,n,t){"use strict";t.r(n);var c=t(1),r=t.n(c),i=t(3),o=t.n(i),s=(t(8),t(9),t(0));var d=function(){return Object(s.jsx)("div",{children:Object(s.jsx)("h1",{children:"hello world"})})};o.a.render(Object(s.jsx)(r.a.StrictMode,{children:Object(s.jsx)(d,{})}),document.getElementById("root"))},8:function(e,n,t){},9:function(e,n,t){}},[[11,1,2]]]);
+//# sourceMappingURL=main.c98105e8.chunk.js.map
