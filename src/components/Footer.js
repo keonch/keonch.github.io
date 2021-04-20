61eacbec8286c89f34280e8369a659b7b536a27b
Keon Choi <keonch91@gmail.com>
2021-04-19 21:13:54 -0400

    create footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,20 @@
 import '../assets/stylesheets/Footer.css';
+import linkedin from '../assets/images/linkedin.png';
+import github from '../assets/images/github.png';
 
 function Footer() {
   return (
     <div className="Footer">
-      Footer
+      <div className="links">
+        <a href="https://www.linkedin.com/in/keon-choi/">
+          <img src={linkedin} alt="linkedin"></img>
+          <div>LinkedIn</div>
+        </a>
+        <a href="https://github.com/keonch">
+          <img src={github} alt="github"></img>
+          GitHub
+        </a>
+      </div>
     </div>
   );
 }
