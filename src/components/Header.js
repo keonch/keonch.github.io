734e3b5dd9958f58df2304e18d0ac2d82df628be
Keon Choi <keonch91@gmail.com>
2021-04-20 16:48:51 -0400

    fix header css

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,16 +6,18 @@ import telephone from '../assets/images/telephone.svg'
 function Header() {
   return (
     <div className="Header">
-      <div className="name">Keon Choi</div>
-      <div className="info">
-        <img src={location}></img>
-        <div className="info-text">New York, NY</div>
+      <div className="header-wrapper">
+        <div className="name">Keon Choi</div>
+        <div className="info">
+          <img src={location}></img>
+          <div className="info-text">New York, NY</div>
 
-        <img src={mail}></img>
-        <div className="info-text">keonch91@gmail.com</div>
+          <img src={mail}></img>
+          <div className="info-text">keonch91@gmail.com</div>
 
-        <img src={telephone}></img>
-        <div className="info-text">646-520-7420</div>
+          <img src={telephone}></img>
+          <div className="info-text">646-520-7420</div>
+        </div>
       </div>
     </div>
   );
