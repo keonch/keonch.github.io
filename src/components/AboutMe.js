71b272acded92f76b14555a516e482b96ce5cd71
Keon Choi <keonch91@gmail.com>
2021-04-21 19:49:43 -0400

    add resume

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -1,5 +1,6 @@
 import '../assets/stylesheets/AboutMe.css';
 import me from '../assets/images/me.png';
+import resume from '../assets/documents/keon_choi_resume.pdf';
 import { summary } from '../assets/documents/summary.js'
 
 function AboutMe() {
@@ -7,7 +8,7 @@ function AboutMe() {
         <div className="AboutMe">
             <img className="me" src={me} alt="me"></img>
             <div className="summary">{summary}</div>
-            <a className="resume-link" target="_blank" rel="noopener noreferrer" href={""}>See Resume</a>
+            <a className="resume-link" target="_blank" rel="noopener noreferrer" href={resume}>See Resume</a>
         </div>
     );
 }
